91786623bfdfc144f907c286b072b10c5d2ebcfe
Jared Zheng <jared@jaredmac.lan>
2013-05-15 08:20:33 -0600

    freshly created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smtet::Application.config.secret_token = '74799e5dc01031b388dbe59a0aead02fd2f4690841a3548c3e187c686dd900df383cb21318e147cc91c73270b2c314a9f4c1402bd473f895a622e20f0fec313e'
